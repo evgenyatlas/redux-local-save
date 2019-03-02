5bbf956e465812c00b2a166769439ae60c9a149b
geckyhit <gekcy@mail.ru>
2019-03-03 00:05:44 +0300

    added options.saveName to support multiple redux store

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ import { SAVE_NAME, SAVE_STORAGE } from './constants'
 
 /**
  * Returns a middleware for redux store
- * @param  {Options} options.
+ * @param {{saveActions: Array, saveName: String, isDebug: Boolean}} options - Extra options
  * @return {function} store => next => action 
  */
 export default function createLocalSaveMiddleware(options) {
     /*Default options*/
     options = options !== undefined ? options : {}
     options.saveActions = options.saveActions !== undefined ? options.saveActions : []
+    options.saveName = options.saveName ? options.saveName : ''
+    options.isDebug = !!options.isDebug
+    /****/
 
     return store => {
 
-        function saveCurrState() {
-            saveInStorage(store.getState())
-        }
+        const saveCurrState = options.isDebug ?
+            () => {
+                console.log('-----SAVE_STATE-----' + options.saveName)
+                console.log(store.getState())
+                saveInStorage(store.getState(), options.saveName)
+                console.log('-------------------')
+            }
+            :
+            () => saveInStorage(store.getState(), options.saveName)
+
 
         return next => {
             return action => {
-                next(action)
+                const result = next(action)
                 if (options.saveActions.some(saveAction => {
                     //such a check, because we get two types of actions
                     //1. DUMB: [TEST_ACTION_1, TEST_ACTION_2] (just a string means type action)
@@ -34,8 +44,10 @@ export default function createLocalSaveMiddleware(options) {
                     if (saveAction._timerId) clearTimeout(saveAction._timerId)
                     saveAction._timerId = setTimeout(saveCurrState, saveAction.debounce)
 
+                    return false
                 }) ||/* default action to save -> */action.type === SAVE_STORAGE)
-                    saveInStorage(store.getState())
+                    saveCurrState()
+                return result
             }
         }
     }
--- utils.js
+++ utils.js
@@ -3,15 +3,19 @@ import { SAVE_NAME } from './constants'
 //TODO: do a crushing facility for a larger state and fix multi SAVE_NAME
 /**
  * Returns saved state from localStorage
- * @return {object}
- *         state
+ * @return {Object} - state
  */
-export function loadStorage() {
-    const localState = window.localStorage.getItem(SAVE_NAME)
+export function loadStorage(saveName = '') {
+    const localState = window.localStorage.getItem(getSaveName(saveName))
     //TODO: Make a check on the object, etc.
     return localState ? JSON.parse(localState) : undefined
 }
 
-export function saveInStorage(state, saveName = SAVE_NAME) {
-    localStorage.setItem(saveName, JSON.stringify(state))
+export function saveInStorage(state, saveName = '') {
+    console.log('saveInStorage', getSaveName(saveName))
+    localStorage.setItem(getSaveName(saveName), JSON.stringify(state))
+}
+
+function getSaveName(name) {
+    return `${SAVE_NAME}_${name}`
 }
